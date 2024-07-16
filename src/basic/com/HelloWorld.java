e137d321ddb22742fa7fb04a3735edfcaa17c698
IMU <IMU@DESKTOP-FSRQE0S>
2024-07-16 09:10:57 +0630

    Version1

---
+++ src/basic/com/HelloWorld.java
@@ -0,0 +1,10 @@
+package basic.com;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello B113 Students");
+	}
+
+}
